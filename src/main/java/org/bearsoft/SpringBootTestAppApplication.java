1721e7e5d71e502df7c26626b83125c72ad7bc3b
Taka <Taka@twhchan@gmail.com>
2018-05-02 22:30:08 +0800

    First commit

---
+++ src/main/java/org/bearsoft/SpringBootTestAppApplication.java
@@ -0,0 +1,12 @@
+package org.bearsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestAppApplication.class, args);
+	}
+}
